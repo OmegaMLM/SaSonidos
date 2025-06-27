d0737d8c65821ff92634fb5fe29a79b0f0dc81d9
OmegaMLM <mateo.marchesinlujan@gmail.com>
2025-06-26 22:36:53 -0300

    Fixing POST method

--- components/forms/FormNewBlog.tsx
+++ components/forms/FormNewBlog.tsx
@@ -2,15 +2,29 @@
 
 import React, { useState } from 'react'
 import {Form, Input, Button} from "@heroui/react";
+import { useFormik } from 'formik';
 
 const FormNewBlog = () => {
-  
+  const formik = useFormik({
+    initialValues: {
+        title: '',
+        content: '',
+        image: '',
+    },
+    onSubmit: values => {
+        alert(JSON.stringify(values, null, 2));
+    },  
+  });
 
   return (
-    <Form
+    <Form onSubmit={formik.handleSubmit}
       className="w-full max-w-xs flex flex-col gap-4"
     >
       <Input
+      label="Titulo"
+      labelPlacement='outside'
+      name='title'
+      placeholder='Ingrese un titulo'
       />
 
       <Input
